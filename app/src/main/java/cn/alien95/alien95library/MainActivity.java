d7fd7f9c860d0c3c015b324c4ad1bfe78b1c37bf
llxal <18983679028@163.com>
2015-12-19 03:03:29 +0800

    12-19-create-project

---
+++ app/src/main/java/cn/alien95/alien95library/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.alien95.alien95library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
