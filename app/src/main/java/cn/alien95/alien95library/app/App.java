9011fb259fcd2c5a4fe23d2a8f9a1eac6c4f30cf
llxdaxia <18983679028@163.com>
2016-01-29 15:13:25 +0800

    gradle

--- app/src/main/java/cn/alien95/alien95library/app/App.java
+++ app/src/main/java/cn/alien95/alien95library/app/App.java
@@ -2,8 +2,6 @@ package cn.alien95.alien95library.app;
 
 import android.app.Application;
 
-import com.facebook.drawee.backends.pipeline.Fresco;
-
 import alien95.cn.http.request.Http;
 import cn.alien95.alien95library.BuildConfig;
 
@@ -16,7 +14,6 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Fresco.initialize(this);
         Http.initialize(this);
         if (BuildConfig.DEBUG) {
             Http.setDebug(true, "NetWork");   //关于网络的调试,日志输出
